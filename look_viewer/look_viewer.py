fc72f09b65a63f1bab4020645f18353b069a54a9
Alexey Dral <aadral@gmail.com>
2017-01-04 00:34:57 +0300

    update project to server static images

--- look_viewer/look_viewer.py
+++ look_viewer/look_viewer.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os, os.path
 
 __version__ = "0.1"
-app = Flask(__name__)
+IMAGE_FOLDER = os.path.join(os.path.dirname(__file__), "static/img")
+app = Flask(__name__, static_url_path="/static")
+
+
+def _get_images(image_folder=IMAGE_FOLDER):
+    filenames = os.listdir(image_folder)
+
+    for filename in filenames:
+        full_path = os.path.join(image_folder, filename)
+        if os.path.isfile(full_path):
+            yield filename
+
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    filenames = list(_get_images())
+    return render_template('index.html', image_collection=filenames)
